449df6773aa0eef116deed455498be0e83ed8f2d
Victor Maciel <victorchapiro@gmail.com>
2022-06-04 15:14:49 -0300

    Criado classe Calculadora - métodos não implementados

---
+++ Calculadora.java
@@ -0,0 +1,19 @@
+public class Calculadora{
+
+        public double resultado;
+    
+        public double soma(){return resultado;}
+    
+        public double subtração(){return resultado;}
+    
+        public double divisão(){return resultado;}
+    
+        public double multiplicação(){return resultado;}
+    
+        public double piso(){return resultado;}
+    
+        public double teto(){return resultado;}
+    
+        public double raizQuadrada(){return resultado;}
+    }
+
