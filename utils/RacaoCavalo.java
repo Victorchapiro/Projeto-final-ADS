4820483bdbc667bdb0c67aa7e44e458b04b954b3
alextoliveira <alextoliveira@edu.unifor.br>
2022-06-04 23:11:18 -0300

    criação da classe RacaoCavalo

---
+++ utils/RacaoCavalo.java
@@ -0,0 +1,15 @@
+package utils;
+
+/**
+ * Para calcular a quantidade (kg) de ração que devemos dar para os cavalos diariamente,
+ * devemos dividir o peso do animal por 50.
+ * @return quantos quilos de ração deve ser colocado para o cavalo
+ */
+
+public class racaoCavalo {
+    
+    public double calculaRacao (int peso){
+        Calculadora calc = new Calculadora();
+        return calc.teto((peso / 50));
+    }
+}
